e8a4413b6dbc914a254013b6195f5e707a737be2
Jerry Ng <ngshiheng@gmail.com>
2023-10-17 09:08:04 +0800

    refactor: make buildUrl and parseResponse

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,34 @@
 export class MASApiService {
     readonly baseUrl: string = "https://eservices.mas.gov.sg/statistics/api/v1";
 
-    private fetch(url: string, params: Record<string, string | number>): GoogleAppsScript.URL_Fetch.HTTPResponse {
-        const paramString = Object.keys(params)
-            .map((key) => `${key}=${params[key]}`)
-            .join("&");
-
-        const fullUrl = encodeURI(`${url}?${paramString}`);
+    private fetch(endpoint: string, params: Record<string, string | number>): any {
+        const url = this.buildUrl(endpoint, params);
+        Logger.log(`Fetching from ${url}`);
 
         const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
             method: "get",
             muteHttpExceptions: true, // Do not throw exceptions for non-200 response codes
         };
 
-        Logger.log(`Fetching: ${fullUrl}`);
+        const response = UrlFetchApp.fetch(url, options);
+        return this.parseResponse(response);
+    }
 
-        const response = UrlFetchApp.fetch(fullUrl, options);
+    private buildUrl(endpoint: string, params: Record<string, string | number>): string {
+        const paramString = Object.keys(params)
+            .map((key) => `${key}=${params[key]}`)
+            .join("&");
+
+        return encodeURI(`${this.baseUrl}/${endpoint}?${paramString}`);
+    }
+
+    private parseResponse(response: GoogleAppsScript.URL_Fetch.HTTPResponse): any {
         const responseBody = response.getContentText();
         return JSON.parse(responseBody);
     }
 
     getSGSBondsIssuanceCalendar(startDate: string, endDate: string, rows: number = 200, sort = "ann_date asc"): any {
-        const endpoint = `${this.baseUrl}/bondsandbills/m/issuancecalendar`;
+        const endpoint = "/bondsandbills/m/issuancecalendar";
         const params = {
             rows: 200,
             filters: `issue_type:("B" OR "I" OR "G") AND ann_date:[${startDate} TO ${endDate}]`,
@@ -38,7 +45,7 @@ export class MASApiService {
         rows: number = 200,
         sort = "ann_date asc",
     ): any {
-        const endpoint = `${this.baseUrl}/bondsandbills/m/issuancecalendar`;
+        const endpoint = "/bondsandbills/m/issuancecalendar";
         const params = {
             rows,
             filters: `issue_type:"T" AND auction_tenor:"${auction_tenor}" AND ann_date:[${startDate} TO ${endDate}]`,
@@ -54,7 +61,7 @@ export class MASApiService {
         rows: number = 200,
         sort = "ann_date asc AND auction_tenor asc AND maturity_date asc",
     ): any {
-        const endpoint = `${this.baseUrl}/bondsandbills/m/cmtbissuancecalendar`;
+        const endpoint = "/bondsandbills/m/cmtbissuancecalendar";
         const params = {
             rows,
             filters: `ann_date:[${startDate} TO ${endDate}]`,
